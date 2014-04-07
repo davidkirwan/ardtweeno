4454db60f85504bc74e96dd57c3ef172d3ca06cb
David Kirwan <00346128@mail.wit.ie>
2014-04-08 00:13:45 +0100

    - Updated gemspec to v0.5.0 - _SKIP_ Updated VERSION inside lib/ardtweeno.rb

--- ardtweeno-gem/lib/ardtweeno.rb
+++ ardtweeno-gem/lib/ardtweeno.rb
@@ -31,7 +31,7 @@ module Ardtweeno
     
     
     # Constants
-    Ardtweeno::VERSION = "0.3.1" unless defined? Ardtweeno::VERSION
+    Ardtweeno::VERSION = "0.5.0" unless defined? Ardtweeno::VERSION
     Ardtweeno::CONFIGPATH = ENV['HOME'] + "/.ardtweeno" unless defined? Ardtweeno::CONFIGPATH
     Ardtweeno::DBPATH = Ardtweeno::CONFIGPATH + "/conf.yaml" unless defined? Ardtweeno::DBPATH
     Ardtweeno::NODEPATH = Ardtweeno::CONFIGPATH + "/nodelist.yaml" unless defined? Ardtweeno::NODEPATH
