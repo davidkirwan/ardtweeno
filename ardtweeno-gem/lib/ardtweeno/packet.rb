dd94078ab8ccdea1eb9bdebc8a15b2ec2455289b
David Kirwan <dkirwan@tssg.org>
2014-08-12 19:29:56 +0000

    - fixes #40 - Ardtweeno is now licenced under the GPL 3.0 licence

--- ardtweeno-gem/lib/ardtweeno/packet.rb
+++ ardtweeno-gem/lib/ardtweeno/packet.rb
@@ -1,9 +1,26 @@
+=begin
 ####################################################################################################
-# @author       David Kirwan <davidkirwanirl@gmail.com>
-# @description  Packet Communication storage class for the Ardtweeno system
+# @author       David Kirwan https://github.com/davidkirwan/ardtweeno
+# @description  Ardtweeno Gateway
 #
-# @date         14-11-2012
+# @date         2014-08-12
 ####################################################################################################
+
+This file is part of Ardtweeno.
+
+Ardtweeno is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+any later version.
+
+Ardtweeno is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+=end
 
 # Imports
 require 'rubygems'
